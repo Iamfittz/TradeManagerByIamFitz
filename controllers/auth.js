fb208380720b93a427107fd03b8f10fbfc4b457b
Iamfitz <vuitsyk.a@gmail.com>
2025-07-19 11:22:48 +0300

    Add errorHandler utility and use it in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
+const utils = require('../utils/errorHandler')
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.login = async function(req,res){
     const candidate = await User.findOne({email:req.body.email})
@@ -47,7 +49,7 @@ module.exports.register = async function(req,res){
             res.status(201).json(user)
         }
         catch(e){
-            
+            errorHandler(res,e)
         }
     }
 }
