156a409f17cfdce6df8690420424e59b0791e388
Iamfitz <vuitsyk.a@gmail.com>
2025-07-19 13:58:29 +0300

    Add initial analytics controller structure

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,7 +1,7 @@
-module.exports.overview = function (reg,res){
+module.exports.overview = function (req,res){
 
 }
 
-module.exports.analytics = function (reg,res){
+module.exports.analytics = function (req,res){
     
 }
