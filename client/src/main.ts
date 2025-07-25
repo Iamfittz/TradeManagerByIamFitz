7b7c9d4007fe7af6d742f32b53e4c1522437df20
Iamfitz <vuitsyk.a@gmail.com>
2025-07-25 18:25:10 +0300

    update

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import 'materialize-css/dist/js/materialize.js'
+import { provideRouter } from '@angular/router';
+import { LoginPage } from './app/login-page/login-page';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter([
+      { path: 'login', component: LoginPage }
+    ])
+  ]
+}).catch(err => console.error(err));
